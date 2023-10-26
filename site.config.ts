db4a2c0ff0e9b33f1723582d2918a742a3d1f1d7
KwangHyeonKim <111857144+rhkdguskim@users.noreply.github.com>
2023-10-26 21:41:19 +0900

    수정2

--- site.config.ts
+++ site.config.ts
@@ -10,8 +10,8 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'BigKwangs Tech Blog',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'KWANG HYEON KIM',
+  domain: 'techblog-dusky.vercel.app',
+  author: 'KWANGHYEONKIM',
 
   // open graph metadata (optional)
   description: 'BigKwangs Tech Blog',
@@ -31,7 +31,7 @@ export default siteConfig({
   defaultPageCoverPosition: 0.3,
 
   // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+  isPreviewImageSupportEnabled: false,
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
